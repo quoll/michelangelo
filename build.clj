45fd6d4875b6e1450e8d4274e1cb4d1f55b9a114
Paula Gearon <pgearon@imohealth.com>
2023-09-29 14:36:24 -0400

    new build process

---
+++ build.clj
@@ -0,0 +1,34 @@
+(ns build
+  (:refer-clojure :exclude [test])
+  (:require [clojure.tools.build.api :as b] ; for b/git-count-revs
+            [org.corfield.build :as bb]))
+
+(def lib 'org.clojars.quoll/michaelangelo)
+(def version "0.1.7")
+
+;; clojure -T:build test
+(defn test "Run the tests." [opts]
+  (bb/run-tests opts))
+
+;; clojure -T:build ci
+(defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
+  (if-not (.exists (java.io.File. "pom.xml"))
+    (throw (ex-info "Must have a pom.xml with a license section" {:missing "./pom.xml"}))
+    (-> opts
+        (assoc :lib lib :version version)
+        (bb/run-tests)
+        (bb/clean)
+        (bb/jar))))
+
+;; clojure -T:build install
+(defn install "Install the JAR locally." [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/install)))
+
+;; clojure -T:build deploy
+(defn deploy "Deploy the JAR to Clojars." [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/deploy)))
+
